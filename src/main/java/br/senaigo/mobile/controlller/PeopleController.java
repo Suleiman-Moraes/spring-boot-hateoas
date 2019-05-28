f12c17d7c0bb4f4e873c8743a1a9b16085cff713
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-05-27 22:32:11 -0300

    Ajustes

--- src/main/java/br/senaigo/mobile/controlller/PeopleController.java
+++ src/main/java/br/senaigo/mobile/controlller/PeopleController.java
@@ -1,90 +1,118 @@
 package br.senaigo.mobile.controlller;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.senaigo.mobile.controlller.abstracts.PadraoController;
+import br.senaigo.mobile.entities.Order;
 import br.senaigo.mobile.entities.People;
+import br.senaigo.mobile.interfaces.GenericOperations;
 import br.senaigo.mobile.interfaces.GenericOperationsController;
+import br.senaigo.mobile.service.OrderService;
+import br.senaigo.mobile.service.PeopleService;
 
 @RestController
 @RequestMapping("/peoples")
-public class PeopleController implements GenericOperationsController<People> {
+public class PeopleController extends PadraoController<People> {
+	@Autowired
+	public PeopleService peopleService;
 	
-	/**
-	 * @see http://appsdeveloperblog.com/spring-boot-logging-with-loggerfactory/
-	 */
-	Logger logger = LoggerFactory.getLogger(PeopleController.class);
-
 	@Override
-	public Resource<People> post(People entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	@PutMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, 
-							 MediaType.APPLICATION_XML_VALUE})
-	@ResponseStatus(HttpStatus.OK)
-	public void put(@RequestBody People people) {
-		logger.info("Atulizado registro de pessoa.");
-		
-	}
-
-	@Override
-	public void delete(People entity) {
-		// TODO Auto-generated method stub
-		
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
+				 produces = {MediaType.APPLICATION_JSON_VALUE,
+						 	 MediaType.APPLICATION_XML_VALUE,
+						 	 MediaTypes.HAL_JSON_VALUE})
+	public ResponseEntity<Resource<People>> post(@RequestBody People people) {
+		try {
+			peopleService.post(people);
+			getLogger().info("Registro inserido");
+			
+			Link link = linkTo(PeopleController.class).slash(people.getIdPeople()).withSelfRel();
+			Resource<People> result = new Resource<People>(people,link);
+			return ResponseEntity.status(HttpStatus.CREATED).body(result);
+		} catch (Exception e) {
+			getLogger().error(String.format("Erro ao executar o método POST.\nMensagem: %s",e.getMessage()));
+			return ResponseEntity.badRequest().body(null);
+		}
 	}
 
 	@Override
 	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, 
 							 MediaType.APPLICATION_XML_VALUE,
 							 MediaTypes.HAL_JSON_VALUE})
-	@ResponseStatus(HttpStatus.OK)
-	public Resources<People> get() {
+	public ResponseEntity<Resources<People>> get(){
 		try {
-			//List<Order> orders = orderService.get();
+			List<People> peoples = peopleService.get();
 			
-			//logger.info(String.format("Registro(s) recuperados(s): %s",orders.toString()));
+			getLogger().info(String.format("Registro(s) recuperados(s): %s",peoples.toString()));
 			
-//			for (final Order order : orders) {
-//		        Link selfLink = linkTo(methodOn(OrderController.class)
-//		          .getOrderById(customerId, order.getIdOrder()())).withSelfRel();
-//		        order.add(selfLink);
-//		    }
-//		  
-//		    Link link = linkTo(methodOn(CustomerController.class)
-//		      .getOrdersForCustomer(customerId)).withSelfRel();
-//		    Resources<Order> result = new Resources<Order>(orders, link);
-//		    return result;
-			
+			for (final Order order : orders) {
+		        Link selfLink = linkTo(OrderController.class)
+		        		.slash(order.getIdOrder())
+		        		.withSelfRel();
+		        
+		        Link selfLinkPeople = linkTo(PeopleController.class)
+		        		.slash(order.getPeople().getIdPeople())
+		        		.withSelfRel();
+		        
+		        order.getPeople().add(selfLinkPeople);
+		        order.add(selfLink);
+		    }
+			 Link link = linkTo(OrderController.class).withSelfRel();
+			 Resources<Order> result = new Resources<Order>(orders, link);
+			 return ResponseEntity.ok(result);
 		} catch (Exception e) {
-			logger.error(String.format("Erro ao executar o método GET.\nMensagem: %s",e.getMessage()));
+			getLogger().error(String.format("Erro ao executar o método GET.\nMensagem: %s",e.getMessage()));
+			return ResponseEntity.badRequest().body(null);
 		}
-		return null;
 	}
 
 	@Override
-	public Resource<People> get(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+	@GetMapping(value="/{id}",
+				consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
+	 			produces = {MediaType.APPLICATION_JSON_VALUE,
+	 						MediaType.APPLICATION_XML_VALUE,
+	 						MediaTypes.HAL_JSON_VALUE})
+	public ResponseEntity<Resource<People>> get(@PathVariable("id") Long id) {
+		try {
+			Order order = orderService.get(Order.builder().idOrder(id).build());
+			getLogger().info(String.format("Registro recuperado: %s",order.toString()));
+			Link link = linkTo(OrderController.class).slash(order.getIdOrder()).withSelfRel();
+			Resource<Order> result = new Resource<Order>(order,link);
+			return ResponseEntity.ok(result);
+		} catch (Exception e) {
+			getLogger().error(String.format("Erro ao executar o método GET.\nMensagem: %s",e.getMessage()));
+			return ResponseEntity.badRequest().body(null);
+		}
 	}
 
 	@Override
-	public void patch(People entity) {
-		// TODO Auto-generated method stub
-		
+	public Class<?> getClassController() {
+		return PeopleController.class;
 	}
 
+	@Override
+	public GenericOperations<People> getGenericOperations() {
+		return peopleService;
+	}
 }
